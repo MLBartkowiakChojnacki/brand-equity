49d4337cc939ab8fb676afcefd35e859ef931958
Krzysztof Chojnacki <kchojnacki@edu.cdv.pl>
2023-01-15 17:08:32 +0100

    2023-01-15

--- brand_equity.py
+++ brand_equity.py
@@ -32,14 +32,8 @@ comb = df_1.merge(df_2, how='cross')
 comb.query('RecordNo == 1135 & COMPANY_CODE == 106')
 
 #%%
-result_dict = {5: {1135:106, 1136:104}
-               , 4: {}
-               , 2: {}
-               , 1: {}}
-
-#%%
 df_0 = df_0.fillna(np.nan)
 a_5 = df_0['Q7M1'][df_0['Q7M1'] != 999].to_frame()
 a_4 = df_0[['Q7M2', 'Q7M3', 'Q7M4', 'Q7M5', 'Q7M6', 'Q7M7', 'Q7M8', 'Q7M9', 'Q7M10']]
-a_5 = {5: a_5.to_dict()
-       , 4: a_4.to_dict()}
+# a_5 = {5: a_5.to_dict()
+#        , 4: a_4.to_dict()}
