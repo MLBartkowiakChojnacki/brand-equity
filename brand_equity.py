aff9fda4044a36d24147a1df247b29a7ff4a0a6f
Krzysztof Chojnacki <kchojnacki@edu.cdv.pl>
2023-01-16 22:58:27 +0100

    2023-01-16

--- brand_equity.py
+++ brand_equity.py
@@ -68,45 +68,38 @@ if __name__ == "__main__":
     
     points_5_q7q8 = df_source[['RecordNo','X3M1']][df_source['X3M1'] != 999]
     points_5_q7q8.columns = ['RECORD_NO', 'COMPANIES']
-    points_5_q7q8['POINTS'] = 5
+    points_5_q7q8['AWARANESS'] = 5
     
     #points_5_q7q8 = get_unique_values(data_frame = points_5_q7q8, selected_columns = ['RECORD_NO', 'COMPANIES'])
     cols = ['X3M2','X3M3','X3M4','X3M5','X3M6','X3M7','X3M8','X3M9','X3M10']
     points_4_q7q8 = df_source[['RecordNo'] + cols].dropna(how='all')
     points_4_q7q8 = get_points_info(data_frame = df_source, columns = cols, index = ['RecordNo'])
     points_4_q7q8.columns = ['RECORD_NO', 'COMPANIES']
-    points_4_q7q8['POINTS'] = 4
+    points_4_q7q8['AWARANESS'] = 4
 
     cols = ['X4M1','X4M2','X4M3','X4M4','X4M5','X4M6','X4M7','X4M8','X4M9','X4M10']   
     points_2_q7q8 = df_source[['RecordNo'] + cols].dropna(how='all')
     points_2_q7q8 = get_points_info(data_frame = df_source, columns = cols, index = ['RecordNo'])
     points_2_q7q8.columns = ['RECORD_NO', 'COMPANIES']
-    points_2_q7q8['POINTS'] = 2
+    points_2_q7q8['AWARANESS'] = 2
   
     df_points = pd.concat([points_5_q7q8, points_4_q7q8, points_2_q7q8], ignore_index=True)
     df_points = df_points.drop_duplicates(subset=['RECORD_NO','COMPANIES'], keep="first")   
     df_points = df_points.query('COMPANIES not in [999, 998]')
     
     points_1_q7q8 = pd.concat([df_points[['RECORD_NO', 'COMPANIES']], df_combined[['RECORD_NO', 'COMPANIES']]]).drop_duplicates(keep=False)
-    points_1_q7q8['POINTS'] = 1
+    points_1_q7q8['AWARANESS'] = 1
     df_points = pd.concat([df_points, points_1_q7q8], ignore_index=True)
     df_points = pd.merge(df_points, companies,  how='left', left_on=['COMPANIES'], right_on = ['COMPANY_CODE'])
     
-#%%
-cols = ['X5M1',	'X5M2',	'X5M3',	'X5M4',	'X5M5',	'X5M6',	'X5M7',	'X5M8',	'X5M9',	'X5M10']
-a = df_source[['RecordNo'] + cols]
-a.columns = ['RecordNo',101,102,103,104,105,106,107,108,109,110]
-
-b = get_points_info(data_frame = a, columns = cols, index = ['RecordNo'])
-#%%
-a.columns[a.columns != 'RecordNo']
-
-#%%
-lista = []
-for row in range(a.shape[0]):
-    for col in a.columns[a.columns != 'RecordNo']:
-        lista.append([a.iloc[row]['RecordNo'], col, a.iloc[row][col]])
-
-aa = pd.DataFrame(lista)
-
-#%%
+    cols_familiarity = ['X5M1',	'X5M2',	'X5M3',	'X5M4',	'X5M5',	'X5M6',	'X5M7',	'X5M8',	'X5M9',	'X5M10']
+    df_familiarity = df_source[['RecordNo'] + cols_familiarity]
+    df_familiarity.columns = ['RecordNo',101,102,103,104,105,106,107,108,109,110]
+    
+    list_familiarity = []
+    for row in range(df_familiarity.shape[0]):
+        for col in df_familiarity.columns[df_familiarity.columns != 'RecordNo']:
+            list_familiarity.append([df_familiarity.iloc[row]['RecordNo'], col, df_familiarity.iloc[row][col]])
+    
+    df_familiarity = pd.DataFrame(list_familiarity, columns = ['RECORD_NO', 'COMPANIES', 'FAMILIARITY'])
+    df_points = pd.merge(df_points, df_familiarity,  how='left', left_on=['RECORD_NO', 'COMPANIES'], right_on = ['RECORD_NO', 'COMPANIES'])
